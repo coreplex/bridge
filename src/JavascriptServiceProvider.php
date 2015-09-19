3e76104ef1def5502260111f308975bc3103e3b7
Michael Jennings <michael.jennings91@gmail.com>
2015-09-19 17:09:04 +0100

    Updated the service provider to be psr-2 compliant

--- src/JavascriptServiceProvider.php
+++ src/JavascriptServiceProvider.php
@@ -1,39 +1,39 @@
-<?php namespace Coreplex\Bridge;
+<?php
+
+namespace Coreplex\Bridge;
 
 use Illuminate\Support\ServiceProvider;
 
-class JavascriptServiceProvider extends ServiceProvider {
+class JavascriptServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['coreplex.bridge.javascript'] = $this->app->share(function($app)
-        {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['coreplex.bridge.javascript'] = $this->app->share(function ($app) {
             return new Javascript;
         });
 
         $this->app->alias('coreplex.bridge.javascript', 'Coreplex\Bridge\Contracts\Javascript');
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['coreplex.bridge.javascript', 'Coreplex\Bridge\Contracts\Javascript'];
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['coreplex.bridge.javascript', 'Coreplex\Bridge\Contracts\Javascript'];
+    }
 }
